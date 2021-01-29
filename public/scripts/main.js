397c4082a28ebc96bf94b124e83580f9d943cfde
Deepkumar Patel <dspguru97@gmail.com>
2021-01-29 18:38:40 -0500

    headerAlmostDone

---
+++ public/scripts/main.js
@@ -0,0 +1,13 @@
+let userIcon = document.querySelector(".fa-user-circle");
+let userPanel = document.querySelector(".user-profile-panel");
+
+function userprofileanim(){
+    console.log("Inside");
+    userPanel.style.right = "90px";
+    userPanel.style.opacity = "1";
+}
+
+// userIcon.addEventListener('click',()=>{
+//     console.log("inside");
+//     userPanel.classList.toggle("userprofileanim");
+// });
