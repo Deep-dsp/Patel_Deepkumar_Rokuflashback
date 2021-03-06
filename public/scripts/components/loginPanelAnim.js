77fe36562189bfe7dcfd55eed6557b9d34eb9c24
Deepkumar Patel <dspguru97@gmail.com>
2021-03-06 17:38:17 -0500

    loginDone

--- public/scripts/components/loginPanelAnim.js
+++ public/scripts/components/loginPanelAnim.js
@@ -5,17 +5,3 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     M.Dropdown.init(elems, {});
   });
-
-// login-register slider
-
-var x = document.querySelector("#login");
-var y = document.querySelector("#register");
-var z = document.querySelector("#btn");
-
-function register(){
-  x.style.left = "-400px";
-  y.style.left = "50px";
-  z.style.left = "110px";
-}
-
-// login-register slider
